4a9aad3f6aff7b659b8889148296e7014a94e4f5
Lars Thorup <lars@zealake.com>
2014-04-18 15:01:42 -0600

    istanbul

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,41 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     gruntConfig.jasmine = {
-        src: [
-            'src/**/*.js',
-            '!src/lib/**/*.js'
-        ],
-        options: {
-            specs: 'test/**/*.test.js',
-            vendor: [
-                'src/lib/**/*.js',
-                'test/lib/**/*.js'
+        src: {
+            src: [
+                'src/**/*.js',
+                '!src/lib/**/*.js'
             ],
-            keepRunner: true
+            options: {
+                specs: 'test/**/*.test.js',
+                vendor: [
+                    'src/lib/**/*.js',
+                    'test/lib/**/*.js'
+                ],
+                keepRunner: true
+            }
         }
     };
     grunt.registerTask('test', ['jasmine:src']);
+
+    gruntConfig.jasmine.istanbul = {
+        src: gruntConfig.jasmine.src.src,
+        options : {
+            specs: gruntConfig.jasmine.src.options.specs,
+            vendor: gruntConfig.jasmine.src.options.vendor,
+            template: require('grunt-template-jasmine-istanbul'),
+            templateOptions: {
+                coverage: 'output/coverage/coverage.json',
+                report: [
+                    {type: 'text-summary'},
+                    {type: 'html', options: {dir: 'output/coverage'}},
+                    {type: 'cobertura', options: {dir: 'output/cobertura'}}
+                ]
+            }
+        }
+    };
+    grunt.registerTask('cover', ['jasmine:istanbul']);
+
 
     grunt.initConfig(gruntConfig);
 };
